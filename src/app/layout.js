ff9942c71998483f30d5ab7694fec98f884cd2ee
Mykyta Yemelianov <kampentarkampentur@gmail.com>
2025-07-17 12:18:45 -0500

    fix build

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { Red_Hat_Display } from "next/font/google";
 import "./globals.css";
 import Header from "@/ui/Header";
@@ -121,19 +122,31 @@ export default async function RootLayout({ children }) {
             }),
           }}
         />
-        <!-- Google Tag Manager -->
-          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5QVX2Z6S');</script>
-        <!-- End Google Tag Manager -->
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5QVX2Z6S');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </Head>
       <body className={redHatDisplay.variable}>
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QVX2Z6S"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) --
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5QVX2Z6S"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ModalProvider>
           <Header cta={cta}/>
           <div style={{ paddingTop: 80 }}>
