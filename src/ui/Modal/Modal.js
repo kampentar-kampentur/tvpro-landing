57e2e4a2477463a60550153509ecce9f7ee9467c
Mykyta Yemelianov <kampentarkampentur@gmail.com>
2025-10-07 15:19:21 -0500

    New Design Fixes

--- src/ui/Modal/Modal.js
+++ src/ui/Modal/Modal.js
@@ -2,8 +2,14 @@ import React from "react";
 import styles from "./Modal.module.css";
 import Button from "@/ui/Button";
 import CloseIcon from "@/assets/icons/close.svg"
+import { useLockScroll } from "./useLockScroll";
+import { useRef } from "react";
+
 
 const Modal = ({ isOpen, onClose, children, className }) => {
+  const modalRef = useRef(null);
+  useLockScroll(isOpen, modalRef);
+
   if (!isOpen) return null;
 
   return (
@@ -12,7 +18,9 @@ const Modal = ({ isOpen, onClose, children, className }) => {
         <Button variant="secondary" size="small" className={styles.closeButton} onClick={onClose}>
           <CloseIcon width="10" heigth="10"/>
         </Button>
-        {children}
+        <div style={{width: '100%', height: '100%'}} ref={modalRef}>
+          {children}
+        </div>
       </div>
     </div>
   );
