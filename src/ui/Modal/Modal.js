3abce1f031dcef1075ce06c8d346ae741b3791e6
Mykyta Yemelianov <kampentarkampentur@gmail.com>
2025-10-29 11:53:39 -0500

    wc_track fix

--- src/ui/Modal/Modal.js
+++ src/ui/Modal/Modal.js
@@ -13,7 +13,7 @@ const Modal = ({ isOpen, onClose, children, className }) => {
   if (!isOpen) return null;
 
   return (
-    <div className={styles.modalOverlay} onClick={onClose}>
+    <div className={`${styles.modalOverlay} modalOverlay`} onClick={onClose}>
       <div className={`${styles.modalContent} ${className}`} onClick={(e) => e.stopPropagation()}>
         <Button variant="secondary" size="small" className={styles.closeButton} onClick={onClose}>
           <CloseIcon width="10" heigth="10"/>
