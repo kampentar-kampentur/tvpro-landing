6ad402ae2df6ce3dabfa2435d1f4612b1cc6cd42
Mykyta Yemelianov <kampentarkampentur@gmail.com>
2026-02-23 21:30:24 -0600

    cloud change

--- src/app/layout.js
+++ src/app/layout.js
@@ -164,7 +164,8 @@ export default async function RootLayout({ children }) {
         {/* Only critical preconnects (max 4) */}
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://res.cloudinary.com" crossOrigin="anonymous" />
+
+        <link rel="preconnect" href="https://media.tvprousa.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://img.youtube.com" />
         <Script id="delayed-marketing-scripts" strategy="afterInteractive">
           {`
