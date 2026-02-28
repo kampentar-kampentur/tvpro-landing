5481eab0e564216b23270d11d6df0d650672dd90
Mykyta Yemelianov <kampentarkampentur@gmail.com>
2026-02-28 13:41:46 -0600

    clarity fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -167,6 +167,15 @@ export default async function RootLayout({ children }) {
 
         <link rel="preconnect" href="https://media.tvprousa.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://img.youtube.com" />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+          `}
+        </Script>
         <Script id="delayed-marketing-scripts" strategy="afterInteractive">
           {`
             (function() {
@@ -193,13 +202,6 @@ export default async function RootLayout({ children }) {
                 'https://connect.facebook.net/en_US/fbevents.js');
                 fbq('init', '809936758465245');
                 fbq('track', 'PageView');
-
-                // Clarity
-                (function(c,l,a,r,i,t,y){
-                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
                 
                 // Google Ads / GA4
                 var gtagScript = document.createElement('script');
