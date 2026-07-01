400f2a871c69027f7f3636b67bd405f31d70e709
Mykyta Yemelianov <kampentarkampentur@gmail.com>
2026-07-01 20:17:38 +0200

    fix: refactor Elfsight widget to use next/script with data-use-service-core for standard SPA/Safari support

--- src/blocks/CustomerReviews/components/ElfsightWidget.js
+++ src/blocks/CustomerReviews/components/ElfsightWidget.js
@@ -1,28 +1,15 @@
 "use client";
-import { useEffect } from "react";
+import Script from "next/script";
 
 export default function ElfsightWidget() {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://static.elfsight.com/platform/platform.js";
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      if (script.parentNode) {
-        script.parentNode.removeChild(script);
-      }
-      if (window.Elfsight) {
-        try {
-          delete window.Elfsight;
-        } catch (e) {
-          window.Elfsight = undefined;
-        }
-      }
-    };
-  }, []);
-
   return (
-    <div className="elfsight-app-0c8768dc-758c-4c7f-a7fe-9fcc2345d890"></div>
+    <>
+      <Script
+        src="https://static.elfsight.com/platform/platform.js"
+        data-use-service-core
+        strategy="afterInteractive"
+      />
+      <div className="elfsight-app-0c8768dc-758c-4c7f-a7fe-9fcc2345d890"></div>
+    </>
   );
 } 
