0903cdbb022b83e500e47a6e9f80c759a3a6dd6f
Mykyta Yemelianov <kampentarkampentur@gmail.com>
2026-07-11 16:11:57 +0200

    seo: add careers page to sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -91,6 +91,12 @@ export default async function sitemap() {
             priority: 0.6,
         },
         {
+            url: `${baseUrl}/careers/`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.6,
+        },
+        {
             url: `${baseUrl}/about/`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
